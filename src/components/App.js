b930786afcde9d5a38abbdcadd84e2b681e3bb98
Eylül Güneş <eylulgist@gmail.com>
2021-07-29 18:53:02 +0300

    implemented Button styled component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
+import Button from "components/common/Button"
+
 function App() {
   return (
-    <h1>App</h1>
+    <>
+      <h1>App</h1>
+      <Button>Test</Button>
+    </>
   );
 }
 
---
+++ src/components/common/Button.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    color: white;
+    background: #f8049c;
+    font-weight: bold;
+    padding: 8px;
+    border-radius: 0px;
+    box-shadow: none;
+    font-size: 1em;
+    width: 100%;
+    display: block;
+    white-space: none;
+
+    &:disabled{
+        background: #eee;
+        color: #666;
+    }
+`;
+
+export default Button;

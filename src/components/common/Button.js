af524c56cf04dc8ff762264b253ba2eeb302e5b8
Eylül Güneş <eylulgist@gmail.com>
2021-07-29 20:32:52 +0300

    bulk styles using the styled-components "css" helper

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,16 +1,25 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Button = styled.button`
   color: white;
   background: ${(props) => (props.secondary ? "black" : "#f8049c")};
   font-weight: bold;
-  padding: 8px;
-  border-radius: 4px;
   box-shadow: none;
-  font-size: 1em;
   width: 100%;
   display: block;
   white-space: none;
+  ${(props) =>
+    props.large
+      ? css`
+          padding: 10px;
+          border-radius: 5px;
+          font-size: 1.5em;
+        `
+      : css`
+          padding: 8px;
+          border-radius: 4px;
+          font-size: 1em;
+        `}
 
   &:disabled {
     background: #eee;
